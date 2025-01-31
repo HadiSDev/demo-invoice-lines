1c1417d1dc477f93105f71e17f91df1c032db065
Hadi Salameh <hadi.salameh@hotmail.com>
2025-01-31 09:08:09 +0100

    update

--- invoice_lines/config.py
+++ invoice_lines/config.py
@@ -8,8 +8,8 @@ SYSTEM_MESSAGE = {
 }
 
 QWEN2_VL_MODELS = {
-    "Qwen2-VL-7B": "Qwen/Qwen2-VL-7B-Instruct",
-    "Qwen2-VL-2B": "Qwen/Qwen2-VL-2B-Instruct",
+    "Qwen2-VL-7B": "Qwen/Qwen2.5-VL-7B-Instruct",
+    "Qwen2-VL-2B": "Qwen/Qwen2.5-VL-2B-Instruct",
 }
 
 DEFAULT_MODEL = QWEN2_VL_MODELS["Qwen2-VL-7B"]
